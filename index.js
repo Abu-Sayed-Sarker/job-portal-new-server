73d9d47cfdd18fc9001a190d38902398f83ab3ae
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2024-12-28 12:05:36 +0600

    server first commot

--- index.js
+++ index.js
@@ -36,7 +36,10 @@ const verifyToken = (req, res, next) => {
     })
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.54bcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
